f1ac838fd590cc29610579ff3163a376e0c586e0
cafooo <73606560+cafooo@users.noreply.github.com>
2025-03-09 18:05:17 +0500

    Update

--- WinDWheel/WinDWheel/urls.py
+++ WinDWheel/WinDWheel/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('miniapp.urls')),
-    url(r'^robots.txt', lambda x: HttpResponse("User-Agent: *\nDisallow:", content_type="text/plain"), name="robots_file"),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain'))
 ]
